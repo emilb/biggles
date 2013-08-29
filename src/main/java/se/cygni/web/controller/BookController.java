f01dab48e718a025bd27c7172be3cca09d513afd
Emil Breding <emil.breding@gmail.com>
2013-08-29 15:04:53 +0200

    Added search by title

--- src/main/java/se/cygni/web/controller/BookController.java
+++ src/main/java/se/cygni/web/controller/BookController.java
@@ -8,12 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import se.cygni.web.model.Book;
-import se.cygni.web.model.Publisher;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,48 +20,48 @@ public class BookController {
 
     private JdbcTemplate jdbcTemplate;
 
-    private static String SQL_BOOK_BY_ID =
-            "SELECT" +
-                    " Böcker.BokID," +
-                    " Böcker.SV1upplaga," +
-                    " Böcker.DennaUpplaga," +
-                    " Böcker.AntalUpplagor," +
-                    " Böcker.RyggNr," +
-                    " Böcker.Konvelut," +
-                    " Böcker.Handling," +
-                    " Författare.Efternamn as FörfattareEfternamn," +
-                    " Författare.Förnamn as FörfattareFörnamn, " +
-                    "Titlar.TitelSV, " +
-                    "Titlar.TitelENG, " +
-                    "Titlar.ENG1upplaga, " +
-                    "Titlar.Chrono, " +
-                    "Illustratörer.Efternamn as IllustratörEfternamn, " +
-                    "Illustratörer.Förnamn as IllustratörFörnamn, " +
-                    "Översättare.Efternamn as ÖversättareEfternamn, " +
-                    "Översättare.Förnamn as ÖversättareFörnamn, " +
-                    "EngFörlag.ENGFörlag, " +
-                    "EngFörlag.ENGkort, " +
-                    "SVFörlag.SVFörlag, " +
-                    "SVFörlag.SVkort, " +
-                    "RevÖversättare.Efternamn as RevÖversättareEfternamn, " +
-                    "RevÖversättare.Förnamn as RevÖversättareFörnamn " +
-                    "FROM " +
-                    "Författare Författare, " +
-                    "Titlar Titlar, " +
-                    "Illustratörer Illustratörer, " +
-                    "Böcker Böcker, " +
-                    "`Översättare` Översättare, " +
-                    "EngFörlag EngFörlag, " +
-                    "SVFörlag SVFörlag, " +
-                    "`Översättare` RevÖversättare " +
-                    "WHERE " +
-                    "Böcker.TitelID = Titlar.TitelID AND " +
-                    "Böcker.FörfattareID = Författare.FörfattareID AND " +
-                    "Böcker.SVFörlag = SVFörlag.SVFörlagID AND " +
-                    "Böcker.`ÖversättareID` = Översättare.ÖversättareID AND " +
-                    "Böcker.RevÖversättareID = RevÖversättare.ÖversättareID AND " +
-                    "Böcker.IllustratörID = Illustratörer.IllustratörID AND " +
-                    "Titlar.ENGFörlag = EngFörlag.ENGFörlagID";
+    private static String SQL_ALL_BOOKS =
+            "SELECT " +
+                "Böcker.BokID, " +
+                "Böcker.SV1upplaga, " +
+                "Böcker.DennaUpplaga, " +
+                "Böcker.AntalUpplagor, " +
+                "Böcker.RyggNr, " +
+                "Böcker.Konvelut, " +
+                "Böcker.Handling, " +
+                "Författare.Efternamn as FörfattareEfternamn, " +
+                "Författare.Förnamn as FörfattareFörnamn, " +
+                "Titlar.TitelSV, " +
+                "Titlar.TitelENG, " +
+                "Titlar.ENG1upplaga, " +
+                "Titlar.Chrono, " +
+                "Illustratörer.Efternamn as IllustratörEfternamn, " +
+                "Illustratörer.Förnamn as IllustratörFörnamn, " +
+                "Översättare.Efternamn as ÖversättareEfternamn, " +
+                "Översättare.Förnamn as ÖversättareFörnamn, " +
+                "EngFörlag.ENGFörlag, " +
+                "EngFörlag.ENGkort, " +
+                "SVFörlag.SVFörlag, " +
+                "SVFörlag.SVkort, " +
+                "RevÖversättare.Efternamn as RevÖversättareEfternamn, " +
+                "RevÖversättare.Förnamn as RevÖversättareFörnamn " +
+            "FROM " +
+                "Författare Författare, " +
+                "Titlar Titlar, " +
+                "Illustratörer Illustratörer, " +
+                "Böcker Böcker, " +
+                "`Översättare` Översättare, " +
+                "EngFörlag EngFörlag, " +
+                "SVFörlag SVFörlag, " +
+                "`Översättare` RevÖversättare " +
+            "WHERE " +
+                "Böcker.TitelID = Titlar.TitelID AND " +
+                "Böcker.FörfattareID = Författare.FörfattareID AND " +
+                "Böcker.SVFörlag = SVFörlag.SVFörlagID AND " +
+                "Böcker.`ÖversättareID` = Översättare.ÖversättareID AND " +
+                "Böcker.RevÖversättareID = RevÖversättare.ÖversättareID AND " +
+                "Böcker.IllustratörID = Illustratörer.IllustratörID AND " +
+                "Titlar.ENGFörlag = EngFörlag.ENGFörlagID ";
 
     @Autowired
     public BookController(DataSource dataSource) {
@@ -81,13 +79,23 @@ public class BookController {
     @RequestMapping("list")
     @ResponseBody
     public List<Book> listBooks() {
-        return this.jdbcTemplate.query(SQL_BOOK_BY_ID, new BookMapper());
+        return this.jdbcTemplate.query(SQL_ALL_BOOKS, new BookMapper());
+    }
+
+    @RequestMapping("search/title/{term}")
+    @ResponseBody
+    public List<Book> searchBooks(@PathVariable String term) {
+        return this.jdbcTemplate.query(createQuery("Titlar.TitelSV", term, true), new BookMapper());
     }
 
     @RequestMapping("list/title")
     @ResponseBody
     public List<String> listTitles() {
         return this.jdbcTemplate.queryForList("select distinct TitelSV from Titlar order by TitelSV asc", String.class);
+    }
+
+    private String createQuery(String column, String param, boolean isLike) {
+        return SQL_ALL_BOOKS + "AND " + column + " LIKE '%" + param + "%'";
     }
 
     public final class BookMapper implements RowMapper<Book> {

9a074a5d52d313b16b875fc7ed38bec229d8350a
Emil Breding <emil.breding@gmail.com>
2013-10-09 15:19:48 +0200

    Steg 2

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-biggles
+Steg 2
 =======
+
+Javascript-testning med Jasmine
+-------------------------------
+
+Vi har lagt till Jasmine som en plugin. Jasmine är konfigurerad att använda goal=test och den
+hookar in sig i testfasen.
+
+I src/main/webapp/resources/js/BigglesApp.js finns en klass som heter BigglesUtil.Paginator. Den
+används för att dela upp en array med objekt i sidor. Ett embryo till test finns i
+src/test/javascript/paginator_spec.js
+
+1. Verifiera att javascripttesterna i nuläget inte går igenom.
+2. Komplettera paginator_spec.js med meningsfulla tester
+3. Säkerställ att hela projektet bygger igen.
+
+Referenser och hjälp:
+http://searls.github.io/jasmine-maven-plugin/
+http://pivotal.github.io/jasmine/
+
+Jasmine har ett BDD mode som startas via:
+```bash
+mvn jasmine:bdd
+```
+
+Öppna sen en webläsare mot http://localhost:8234/ så får du kontinuerliga uppdateringar av testresultaten.
+
+Övriga kommandon:
+```bash
+mvn jasmine:test
+mvn test
+```
--- src/test/javascript/paginator_spec.js
+++ src/test/javascript/paginator_spec.js
@@ -8,32 +8,34 @@ describe('the Paginator object',function() {
     describe ('should calculate number of pages', function() {
 
         it('correctly when items evenly matches page size',function() {
+
+            // Paginator takes an array and the pagesize
             var paginator = new BigglesUtil.Paginator(dataArray, 10);
             expect(paginator.noOfPages).toBe(10);
         });
 
         it('correctly when items unevenly matches page size',function() {
             var paginator = new BigglesUtil.Paginator(dataArray.slice(1, 27), 10);
-            expect(paginator.noOfPages).toBe(3);
+            expect(false).toBe(true);
         });
     });
 
     describe ('should calculate number of items in current page', function() {
 
         it('correctly when items evenly matches page size',function() {
+            // Verify that paginator.itemsInCurrentPage and paginator.currentPage
+            // are correct when items match pagecount evently
             var paginator = new BigglesUtil.Paginator(dataArray, 10);
             paginator.setPage(10);
 
-            expect(paginator.itemsInCurrentPage.length).toBe(10);
-            expect(paginator.currentPage).toBe(10);
+            expect(paginator.itemsInCurrentPage.length).toBe(false);
+            expect(paginator.currentPage).toBe(false);
         });
 
         it('correctly when items unevenly matches page size',function() {
-            var paginator = new BigglesUtil.Paginator(dataArray.slice(1,27), 10);
-            paginator.setPage(3);
-
-            expect(paginator.itemsInCurrentPage.length).toBe(6);
-            expect(paginator.currentPage).toBe(3);
+            // Verify that paginator.itemsInCurrentPage and paginator.currentPage
+            // are correct on the last page when items doesn't match pagecount evently
+            expect(false).toBe(true);
         });
     });
 
